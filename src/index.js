a60607580d756235dee41e61351d46c3a928a3e7
Arseniya <goldstandart165@yandex.ru>
2018-02-28 12:04:54 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if(!str) return undefined;
+  
+	var array = [];
+  var openedBrackets = { '{': '}', '[': ']', '(': ')' };
+  var closedBrackets = { '}': true, ']': true, ')': true };
+  var element;
+  
+  for (var i = 0; i < str.length; i ++) {
+    element = str[i];
+    if (openedBrackets[str[i]]) {
+      array.push(str[i])
+    }
+    else if (closedBrackets[str[i]]) {
+      if (openedBrackets[array.pop()] !== str[i]) return false
+    }
+  }
+  return array.length === 0;
 }
